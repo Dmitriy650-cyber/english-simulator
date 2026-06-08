d79ff13cf4998c5d7697e13cdb93da938033cb8c
Dmitriy650-cyber <dmitriy_babkov@bk.ru>
2026-06-08 12:00:49 +0300

    Заголовок перенесен из окна на страницу

--- EnglishSimulator.Desktop/ViewModels/DeckViewModel.cs
+++ EnglishSimulator.Desktop/ViewModels/DeckViewModel.cs
@@ -7,5 +7,27 @@
 			"item 2",
 			"item 3"
 			];
+
+		#region Свойства
+
+		#region Заголовок страницы
+
+		/// <summary>
+		/// Заголовок страницы
+		/// </summary>
+		private string? _Caption = "DECKS";
+
+		/// <summary>
+		/// Заголовок страницы
+		/// </summary>
+		public string? Caption
+		{
+			get => _Caption;
+			set => Set(ref _Caption, value);
+		}
+
+		#endregion
+
+		#endregion
 	}
 }
